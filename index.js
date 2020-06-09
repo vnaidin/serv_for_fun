9c6286eca98d29fd7f74b605bd5e1b4302a34324
Vladyslav Naidin <vlad_naidin@outlook.com>
2020-06-09 11:09:28 +0200

    temporarily clean public folder

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var rp = require("request-promise");
 var $ = require("cheerio");
 var express = require("express");
-var os = require("os")
+var os = require("os");
 var bodyParser = require("body-parser");
-var extractPlaylist = require("./functions/extractPlaylist");
-var writeData = require("./functions/writeData");
-var createPlaylistFile = require("./functions/createPlaylistFile")
-
+var createPlaylistFile = require("./functions/createPlaylistFile");
 
 var app = express();
 var host = "0.0.0.0";
@@ -27,21 +24,21 @@ app.use(allowCrossDomain);
 
 /* TODO: add public folder with  html player, and possibility to switch the channels*/
 
-app.listen( port,host, function(){
-console.log(`Server started at ${new Date().toTimeString()} \nListening on ${Object.values(os.networkInterfaces())[0][1].address}:${port}`)});
+app.listen(port, host, function () {
+  console.log(
+    `Server started at ${new Date().toTimeString()} \nListening on ${
+      Object.values(os.networkInterfaces())[0][1].address
+    }:${port}`
+  );
+});
 
 // app.use(express.static('public'))/* everything that is in this folder, wil be presented => TODO: s*/
 
-
-//var arrOfLinks = [];
-//var templateToWrite = [];
-
 app.get("/playlist", (req, res) => {
-    //console.log("TODO: fix problem with Ukraina and Footballs \npossible errors are: \nhttps, \nlong token with id,pid and other params \nand it is working in VLC")
-    console.log(`Requested playlist at ${new Date().toTimeString()}`);
-    console.log("Headers of requesting device:")
-    console.log(req.headers);
-    res.download("./data/playlist.m3u", "playlist.m3u")
+  //"TODO: fix problem with Ukraina and Footballs , possible errors are: https, long token with id,pid and other params <= why it is working in VLC?")
+  console.log(`Requested playlist at ${new Date().toTimeString()}`);
+  //  createPlaylistFile("http://telego477.com");
+  res.download("./data/playlist.m3u", "playlist.m3u");
 });
 
-setInterval(()=>createPlaylistFile("http://telego477.com"),1000*60*15);
+setInterval(() => createPlaylistFile("http://telego477.com"), 1000 * 60 * 15);
